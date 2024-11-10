7c5b31ae122d9dd52017e80277279931bdba3c91
amisg <117622196+amisg@users.noreply.github.com>
2024-11-11 00:53:19 +0530

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const {
 	initializeDatabase,
 	listTransactions,
+	getStatistics,
+	getBarChart,
+	getPieChart,
+	combinedData,
 } = require("../controllers/product.controller");
 
 const router = express.Router();
@@ -9,5 +13,13 @@ const router = express.Router();
 router.get("/initialize-database", initializeDatabase);
 
 router.get("/transactions", listTransactions);
+
+router.get("/statistics", getStatistics);
+
+router.get("/barChart", getBarChart);
+
+router.get("/pieChart", getPieChart);
+
+router.get("/combinedData", combinedData);
 
 module.exports = router;
